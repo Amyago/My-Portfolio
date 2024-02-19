68fa406d92ee5b6058f69c90310023642fb4092c
Amyago <90687633+Amyago@users.noreply.github.com>
2024-02-19 10:53:32 +0300

    Update perceptron for convert Celsius to Fahrenheit.py

--- Neural networks/Regression/perceptron for convert Celsius to Fahrenheit.py
+++ Neural networks/Regression/perceptron for convert Celsius to Fahrenheit.py
@@ -5,20 +5,26 @@ from tensorflow import keras
 from tensorflow.keras.layers import Dense
 
 # Обучающая выборка
-c = np.array([-40, -10, 0, 8, 15, 22, 38])
-f = np.array([-40, 14, 32, 46, 59, 72, 100])
+c = np.array([-40, -10, 0, 8, 15, 22, 38]) # градусы Цельсия
+f = np.array([-40, 14, 32, 46, 59, 72, 100]) # градусы Фаренгейта
 
+# Создание нейронной сети
 model = keras.Sequential()
 model.add(Dense(units=1, input_shape=(1,), activation='linear'))
 
+# Компилирование НС
 model.compile(loss='mean_squared_error', optimizer=keras.optimizers.Adam(0.1))
 
+# Обучение НС
 history = model.fit(c, f, epochs=500, verbose = False)
 
+# Построение графика критерия качества
 plt.plot(history.history['loss'])
 plt.grid(True)
 plt.show()
 
+# Прогнозирование значения
 print(model.predict([100]))
 
+# Вывод весов НС
 print(model.get_weights())
